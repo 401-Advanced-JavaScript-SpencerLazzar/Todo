29ff48d39b80d9b5c8aa5571688d0ffee0a0a7c3
Spencer Lazzar <spencerlazzar@yahoo.com>
2020-11-23 10:43:59 -0800

    Basic File Setup

---
+++ src/Components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+      <h6>This is the form</h6>
+    </div>
+  )
+}
+
+export default Form
---
+++ src/Components/List.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const List = () => {
+  return (
+    <div>
+      <h6>This will be the List</h6>
+    </div>
+  )
+}
+
+export default List
