b1c75341fd7b8b6326f4696ff817fc2e20f05714
Spencer Lazzar <spencerlazzar@yahoo.com>
2020-11-23 19:28:09 -0800

    Monday blues

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
-import Footer from './Components/Footer';
-import Form from './Components/Form';
-import Header from './Components/Header';
-import List from './Components/List';
+import ToDo from './Components/todo-connected';
+
 
 function App() {
   return (
     <div className="App">
-      <Header>
-        <Header />
-      </Header>
-      <Form />
-      <List />
-      <Footer>
-        <Footer />
-      </Footer>
+      <ToDo />
     </div>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,50 @@
-import React from 'react'
 
-const Form = () => {
-  return (
-    <div>
-      <h6>This is the form</h6>
-      <p> I need to change something</p>
+import React from 'react';
 
-    </div>
-  )
+class TodoForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { item: {} };
+  }
+  handleInputChange = e => {
+    this.setState({ item: { ...this.state.item, [e.target.name]: e.target.value } });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    this.props.handleSubmit(this.state.item);
+    const item = {};
+    this.setState({ item });
+  };
+
+  render() {
+    return (
+      <>
+        <h3>Add Item</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <span>To Do Item</span>
+            <input
+              name="text"
+              placeholder="Add To Do List Item"
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label>
+            <span>Difficulty Rating</span>
+            <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={this.handleInputChange} />
+          </label>
+          <label>
+            <span>Assigned To</span>
+            <input type="text" name="assignee" placeholder="Assigned To" onChange={this.handleInputChange} />
+          </label>
+          <button>Add Item</button>
+        </form>
+      </>
+    );
+  }
 }
 
-export default Form
+export default TodoForm;
